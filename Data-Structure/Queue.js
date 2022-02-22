a84ff887c77276c80bcf79e7a88efca44653415a
Seung-wan <tmddhks0104@gmail.com>
2022-02-22 23:35:46 +0900

    2022-02-22

--- Data-Structure/Queue.js
+++ Data-Structure/Queue.js
@@ -1,21 +1,37 @@
-class Queue {
-  constructor() {
+// class Queue {
+//   constructor() {
+//     this.queue = [];
+//   }
+//   enqueue(item) {
+//     const length = this.queue.push(item);
+//     return length;
+//   }
+
+//   dequeue() {
+//     const item = this.queue.shift();
+//     return item;
+//   }
+
+//   entries() {
+//     console.log(this.queue);
+//   }
+// }
+let Queue = (function Queue() {
+  function Queue() {
     this.queue = [];
   }
-  enqueue(item) {
-    const length = this.queue.push(item);
-    return length;
-  }
-
-  dequeue() {
-    const item = this.queue.shift();
-    return item;
-  }
-
-  entries() {
+  Queue.prototype.enqueue = function (item) {
+    this.queue.push(item);
+  };
+  Queue.prototype.dequeue = function () {
+    this.queue.shift();
+  };
+  Queue.prototype.entries = function () {
     console.log(this.queue);
-  }
-}
+  };
+
+  return Queue;
+})();
 
 const queue = new Queue();
 queue.enqueue(1);
